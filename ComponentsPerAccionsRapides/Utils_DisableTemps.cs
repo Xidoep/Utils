ab81c7fc40f65c34d77aaa53c7a4576d13cedc47
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2021-11-02 16:34:09 +0100

    Canviar DisableTemps
    S'ha canviat l'script perque no utilitzi AnimacioPerCodi, ja n'hi ha un que l'utilitza al mateix package, de APC

--- ComponentsPerAccionsRapides/Utils_DisableTemps.cs
+++ ComponentsPerAccionsRapides/Utils_DisableTemps.cs
@@ -1,17 +1,12 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using XS_Utils;
 
-public class Utils_DisableTemps : AnimacioPerCodi_Base
+public class Utils_DisableTemps : MonoBehaviour
 {
     [SerializeField] bool enEnable;
     [SerializeField] float tempsDisable;
-    [SerializeField] [Tooltip("Animacio que fara mentre espera a Disoldres")] AnimacioPerCodi.Animacio animacio;
-
-    [SerializeField] AnimacioPerCodi_All.T_All[] transformacions;
-    internal override Transformacions[] GetTransformacions => transformacions;
-
     private void OnEnable()
     {
         if (!enEnable)
@@ -22,28 +17,6 @@ public class Utils_DisableTemps : AnimacioPerCodi_Base
 
         Disable(tempsDisable);
     }
-
-    public void Disable() 
-    { 
-        gameObject.SetActive(false, tempsDisable);
-
-        StartCoroutine(animacio.Play(transform));
-        Play();
-
-    }
-    public void Disable(float temps) 
-    {
-        gameObject.SetActive(false, temps);
-
-        StartCoroutine(animacio.Play(transform));
-        Play();
-    }
-
-    /*public override void Transformar(float temps) 
-    {
-        if (GetTransformacions.Length == 0)
-            return;
-
-        GetTransformacions[0].Transformar(transform, temps);
-    } */
+    public void Disable() => gameObject.SetActive(false, tempsDisable);
+    public void Disable(float temps) => gameObject.SetActive(false, temps);
 }
