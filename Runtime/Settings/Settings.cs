0bff2950758189b66e257b2f38856db11f7d900b
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2021-12-16 18:03:34 +0100

    Fer settings per classe

--- Runtime/Settings/Settings.cs
+++ Runtime/Settings/Settings.cs
@@ -5,34 +5,35 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "XS/Utils/Settings", fileName = "Settings")]
 public class Settings : ScriptableObject
 {
-    [SerializeField] [Range(0.75f, 1.5f)] float uiSize  = 1;
-    System.Action<float> enUiSize;
 
-    public void UiSize_Set(float valor)
-    {
-        uiSize = valor;
-        enUiSize?.Invoke(uiSize);
-    }
-    public float UISize_Get() => uiSize;
+    /*[SerializeField] [Range(0.75f, 1.5f)] float uiSize  = 1;
+System.Action<float> enUiSize;
 
-    public void UiSize_AddEvent(System.Action<float> action) => enUiSize += action;
-    public void UiSize_AddAndInvokeEvent(System.Action<float> action) 
-    {
-        action.Invoke(uiSize);
-        UiSize_AddEvent(action);
-    } 
-    public void UiSize_RemoveEvent(System.Action<float> action) => enUiSize -= action;
+public void UiSize_Set(float valor)
+{
+uiSize = valor;
+enUiSize?.Invoke(uiSize);
+}
+public float UISize_Get() => uiSize;
 
-
-    private void OnValidate()
-    {
-        tamanyInterficie?.Event_Invoke();
-        enUiSize?.Invoke(uiSize);
-    }
+public void UiSize_AddEvent(System.Action<float> action) => enUiSize += action;
+public void UiSize_AddAndInvokeEvent(System.Action<float> action) 
+{
+action.Invoke(uiSize);
+UiSize_AddEvent(action);
+} 
+public void UiSize_RemoveEvent(System.Action<float> action) => enUiSize -= action;
 
 
-    public Variable<float> tamanyInterficie;
-    public void TamanyInterficie_Set(float value) => tamanyInterficie.Value_Set(value);
+private void OnValidate()
+{
+tamanyInterficie?.Event_Invoke();
+enUiSize?.Invoke(uiSize);
+}
+
+*/
+    public Variable<float> interfaceSize; //1
+     [Tooltip("Entre: 0.75f i 1.5f")]public void InterfaceSize(float value) => interfaceSize.Set(value);
 
 
     [System.Serializable]
@@ -41,12 +42,12 @@ public class Settings : ScriptableObject
         public T value;
         System.Action<T> onValueChange;
 
-        public void Value_Set(T value)
+        public void Set(T value)
         {
             this.value = value;
             onValueChange?.Invoke(this.value);
         }
-        public T Value_Get() => value;
+        public T Get() => value;
 
 
 
