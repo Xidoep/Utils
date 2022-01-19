56915b2eb69d997ae87da99a6f43628fbb4308b8
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2022-01-19 17:51:46 +0100

    Add opcions per controlar particules

--- Runtime/Utils_Funcions.cs
+++ Runtime/Utils_Funcions.cs
@@ -776,14 +776,34 @@ namespace XS_Utils
 
     public static class Particules
     {
-        public static void Emetre(this ParticleSystem particleSystem, int count, float time)
+        static ParticleSystem.MainModule mainModule;
+        static ParticleSystem.EmissionModule emissionModule;
+        public static void Emit(this ParticleSystem particleSystem, int count)
+        {
+            ParticleSystem.EmitParams emitParams =  new ParticleSystem.EmitParams();
+            particleSystem.Emit(emitParams, count);
+        }
+        public static void Emit(this ParticleSystem particleSystem, int count, float time)
         {
             //particleSystem.Params
-            ParticleSystem.EmitParams emitParams =  new ParticleSystem.EmitParams();
-            var mainModule = particleSystem.main;
+            ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+            mainModule = particleSystem.main;
             mainModule.startLifetime = time;
             particleSystem.Emit(emitParams, count);
         }
+        public static void EmisionRateOverTime(this ParticleSystem particleSystem, float count)
+        {
+            emissionModule = particleSystem.emission;
+            emissionModule.rateOverTime = new ParticleSystem.MinMaxCurve(count);
+        }
+        public static void StopAction(this ParticleSystem particleSystem, ParticleSystemStopAction action)
+        {
+            mainModule = particleSystem.main;
+            mainModule.stopAction = action;
+        }
+        public static void Stop(this ParticleSystem particleSystem) => particleSystem.Stop();
+
+        public static bool IsEmitting(this ParticleSystem particleSystem) => particleSystem.isEmitting;
     }
 
     public static class Traduccio
