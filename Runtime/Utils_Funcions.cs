624486c0269656db2c43d48c0aa19696bf86c2ba
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2022-01-21 16:04:02 +0100

    asd

--- Runtime/Utils_Funcions.cs
+++ Runtime/Utils_Funcions.cs
@@ -283,7 +283,7 @@ namespace XS_Utils
     public static class XS_Layers
     {
         public static LayerMask Everything => -1;
-
+        public static int GetLayer(string name) => LayerMask.NameToLayer(name);
         public static bool Contains(this LayerMask layerMask, int layer) => (layerMask.value & (1 << layer)) > 0;
     }
 
