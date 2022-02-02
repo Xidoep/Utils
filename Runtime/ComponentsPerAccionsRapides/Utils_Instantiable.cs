73989c859cd671782c78d702fbe84451c1d6e505
Xidostudio <45095517+Xidoep@users.noreply.github.com>
2022-02-02 17:09:56 +0100

    Instanciable

---
+++ Runtime/ComponentsPerAccionsRapides/Utils_Instantiable.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XS_Utils;
+
+public class Utils_Instantiable : MonoBehaviour
+{
+    Vector3 position;
+    Quaternion rotation;
+
+    Vector3 Position() => position;
+    Quaternion Rotation() => rotation;
+
+
+
+    public void Instantiate() => gameObject.Instantiate();
+
+    public void Instantiate(Vector3 position)
+    {
+        this.position = position;
+
+        gameObject.Instantiate(Position);
+    }
+
+    public void Instantiate(Vector3 position, Quaternion rotation)
+    {
+        this.position = position;
+        this.rotation = rotation;
+
+        gameObject.Instantiate(Position, Rotation);
+    }
+
+    public void Instantiate(Vector3 position, Quaternion rotation, Transform transform)
+    {
+        this.position = position;
+        this.rotation = rotation;
+
+        gameObject.Instantiate(Position, Rotation, transform);
+    }
+    public void Instantiate(Vector3 position, Quaternion rotation, float uniformScale)
+    {
+        this.position = position;
+        this.rotation = rotation;
+
+        gameObject.Instantiate(Position, Rotation).transform.localScale = Vector3.one * uniformScale;
+    }
+}
