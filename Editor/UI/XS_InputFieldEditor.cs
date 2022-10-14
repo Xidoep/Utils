608532ea2d2c8aad491f49feae344093dbf77186
xeviegido <Xidostudio@gmail.com>
2022-10-14 17:33:21 +0200

    a mitg fer el inputfield personalitzat

---
+++ Editor/UI/XS_InputFieldEditor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using TMPro.EditorUtilities;
+
+[CustomEditor(typeof(XS_Inputfield))]
+[CanEditMultipleObjects]
+public class XS_InputFieldEditor : TMP_InputFieldEditor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Runtime/UI/XS_Inputfield.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class XS_Inputfield : TMP_InputField
+{
+    public TMP_InputField nextInputField;
+}
