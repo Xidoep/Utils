0cd0753c744cda8d4b874c6ba1a28b800ebe97fd
xeviegido <Xidostudio@gmail.com>
2023-02-09 18:31:50 +0100

    borrar duplicats

--- Runtime/UI/XS_Inputfield.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using TMPro;
-
-public class XS_Inputfield : TMP_InputField
-{
-    public TMP_InputField nextInputField;
-}
