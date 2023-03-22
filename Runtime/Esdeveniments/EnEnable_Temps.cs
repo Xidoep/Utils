9558d65dfc897782b2914fa48ee66c65b0db5d89
xeviegido <Xidostudio@gmail.com>
2023-03-22 16:42:46 +0100

    frame dependant

--- Runtime/Esdeveniments/EnEnable_Temps.cs
+++ Runtime/Esdeveniments/EnEnable_Temps.cs
@@ -7,11 +7,18 @@ using XS_Utils;
 public class EnEnable_Temps : MonoBehaviour
 {
     [SerializeField] float temps;
+    [SerializeField] bool frameDependent;
     [SerializeField] UnityEvent esdeveniment;
 
     public UnityEvent Esdeveniment => esdeveniment;
 
-    private void OnEnable() => XS_Coroutine.StartCoroutine_Ending(temps, Invocar);
+    private void OnEnable() 
+    {
+        if(!frameDependent)
+            XS_Coroutine.StartCoroutine_Ending(temps, Invocar);
+        else
+            XS_Coroutine.StartCoroutine_Ending_FrameDependant(temps, Invocar);
+    } 
 
     void Invocar() => esdeveniment.Invoke();
 }
